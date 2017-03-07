f605b87b96f0ef360706ac723fd3698ed29b8efc
stefan22 <stefan.emailacct@gmail.com>
2017-03-07 00:48:24 +0000

    function core

--- styleInjection.js
+++ styleInjection.js
@@ -1,71 +1,57 @@
 (function() {
+
+var arr = [];
+var inner = null;
+
+
+function styleInner(arr) {
 	
+	//convert into string
+	var css = arr.toString();
+	//removes white space
+	css = css.replace(/\s+/g,'');
+	
+	var re1 = /}./gi;
+	var re2 = /}#/gi;
 
+	for (var i=0 i < css.length; i++) {
+		//convert class
+		css[i].replace(re1,'},.');
+		arr.push(css[i];)
+		//convert id
+		css[i].replace(re2,'},#');
+		arr.push(css[i]);
+	}
 
-#popup {
-	width:500px;
-	height: 200px;
-	border:5px solid red;
+	console.log(arr);
+	inner = "style.innerHTML = " + arr + ";";
+	return inner;
 }
 
 
-.overlay {
-	
-	position:absolute;
-	top:0;
-	left:0;
-}
 
-
-function(css) {
-	
-	styleTags();
-	styleInner();
-	styleHead();
+function render() {
+	//style elem
+	var style = document.createElement('style');	
+	//style attr
+	style.type = "text/css";
+	if (inner != null) {
+		return inner;
+	}
+	//append to head
+	document.getElementsByTagName('head')[0].appendChild(style);
 
 }
 
-/*************
 
-styleInjection
+function css(css) {
 
-**************/
+	styleInner(css);
+	setTimeout(function() {
+		render();
+	},3000);
 
-var style = document.createElement('style');
-style.type = 'text/css';
-style.innerHTML = "#popup {width:500px; height:200px; border:5px solid #383838; z-index:2000; background-color:rgba(255,255,255,.95); border:1px inset #0e0e0e;outline:3px solid #c7c7c7; padding:15px;}";
-style.innerHTML += "#overlayOff {display:none;}";
-style.innerHTML += ".close {font-size:10px;line-height:16px; padding:7px 9px; position: absolute; top:10px; right: 10px; text-shadow:0px 0px 1px black; border-radius:3px; background-color:#09223c; box-shadow:0px 0px 1px rgba(0,0,0,.35); float:right;margin-right:5px; margin-top:5px; color:#fff; text-transform:uppercase; letter-spacing:1px; font-family:arial,helvetica,sans-serif; cursor:pointer;}";
-style.innerHTML += ".close:hover {background-color:#2121c0;}";
-style.innerHTML += ".close:active,.close:focus,.close:visited {outline:none!important;}";
-style.innerHTML += ".heading1 {color:#002776; font-size: 28px; line-height: 36px; margin-top:10px !important; font-weight:600; letter-spacing:.5px; margin-left:0 !important;}";
-style.innerHTML += ".pop1 {font-size:15px; color:#646161; line-height: 24px; margin-top:.75em;}";
-style.innerHTML += "#gtouch { margin-top:30px; height:55px; vertical-align: bottom;}";
-style.innerHTML += "#gtouch img {width:40px; height:40px; margin-right:15px;float:left; background:#0c79d4; color:#fff;}";
-style.innerHTML += "#slogan {float:left; color:#646161;}";
-style.innerHTML += "#phone {float:left; color:#002776; letter-spacing:1px; font-weight:600; line-height:20px; font-size:19px; text-decoration:none;}";
-//append to head
-document.getElementsByTagName('head')[0].appendChild(style);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
 
 })(); 
